a1a136d6f57b09c0f4b4afb11170c1b85913a80d
satyowicaksana <satyo.wicaksana@rebelworks.co>
2021-03-12 23:14:28 +0700

    initialize react with typescript and antd

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Button } from 'antd';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type='primary'>Wasted Nights</Button>
+    </div>
+  );
+}
+
+export default App;
