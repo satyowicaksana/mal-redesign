7d505a1ca1f67374c6e2a154f138fbef028658b4
satyowicaksana <satyo.wicaksana@rebelworks.co>
2021-03-13 23:21:24 +0700

    add carousel functionality

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,18 +2,30 @@ import { useState } from 'react';
 import { Row, Col, Typography, Tag } from 'antd';
 import { FaChevronLeft, FaChevronRight, FaRegCalendarAlt } from 'react-icons/fa'
 
+import { useInterval } from 'helpers/hooks';
 import './Carousel.less';
 
 const { Paragraph, Title } = Typography
 
 type CarouselProps = {
-  items: string[];
+  items: {
+    imagePath: string;
+    title: string;
+    description:string;
+  }[];
 }
 
 const Carousel = ({
   items
 }: CarouselProps) => {
   const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+  const [autoplay, setAutoplay] = useState(true);
+
+  useInterval(() => {
+    if(autoplay) {
+      handleClickRight();
+    }
+  }, 4000);
 
   const handleClickRight = () => {
     if(selectedItemIndex === items.length - 1) {
@@ -32,54 +44,54 @@ const Carousel = ({
   }
 
   return (
-      <div className='carousel-wrapper'>
+      <div className='carousel-wrapper' onMouseEnter={() => setAutoplay(false)} onMouseLeave={() => setAutoplay(true)}>
         { items.map((item, i) => (
-          <div key={i} className={`carousel-card ${selectedItemIndex === i ? 'selected' : ''}`}>
-            <Row>
-              <Col span={14}>
-                <img src='https://i2.wp.com/anigate.id/wp-content/uploads/2019/03/fate-grand-order-camelot-e1553412113985.jpg?fit=1200%2C807&ssl=1' alt='' className='carousel-card-image' />
-              </Col>
-              <Col span={10} className='carousel-card-info-container p-3'>
-                <div>
-                  <Row className='mb-2'>
-                    <Col>
-                      <Tag color='blue'>FEATURED</Tag>
-                    </Col>
-                    <Col>
-                      <Tag color='blue'>NEWS</Tag>
-                    </Col>
-                  </Row>
-                  <Title level={3} ellipsis={{rows: 3}} className='mb-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Title>
-                  <Paragraph ellipsis={{rows: 5}} className='mb-2'>Lorem ipsum dolor sit amet</Paragraph>
-                  <Row gutter={8} align='middle' className='mb-2'>
-                    <Col className='carousel-calendar-icon-container'>
-                      <FaRegCalendarAlt className='carousel-calendar-icon'/>
-                    </Col>
-                    <Col>
-                      12/03/2021
-                    </Col>
-                  </Row>
-                </div>
-                <Row gutter={8}>
-                  <Col>
-                    <div className='carousel-pointer selected'/>
+          <img key={i} src={items[i].imagePath} alt='' className={`carousel-background-image ${selectedItemIndex === i ? 'selected' : ''}`} />
+        )) }
+        <div className='content-container pt-4 pb-5'>
+          <div className='carousel-cards-container'>
+            { items.map((item, i) => (
+              <div key={i} className={`carousel-card ${selectedItemIndex === i ? 'selected' : ''}`}>
+                <Row>
+                  <Col span={14}>
+                    <img src={item.imagePath} alt='' className='carousel-card-image' />
                   </Col>
-                  <Col>
-                    <div className='carousel-pointer'/>
-                  </Col>
-                  <Col>
-                    <div className='carousel-pointer'/>
-                  </Col>
-                  <Col>
-                    <div className='carousel-pointer'/>
+                  <Col span={10} className='carousel-card-info-container p-3'>
+                    <div>
+                      <Row className='mb-2'>
+                        <Col>
+                          <Tag color='blue'>FEATURED</Tag>
+                        </Col>
+                        <Col>
+                          <Tag color='blue'>NEWS</Tag>
+                        </Col>
+                      </Row>
+                      <Title level={3} ellipsis={{rows: 3}} className='mb-2'>{item.title}</Title>
+                      <Paragraph ellipsis={{rows: 5}} className='mb-2'>{item.description}</Paragraph>
+                      <Row gutter={8} align='middle' className='mb-2'>
+                        <Col className='carousel-calendar-icon-container'>
+                          <FaRegCalendarAlt className='carousel-calendar-icon'/>
+                        </Col>
+                        <Col>
+                          12/03/2021
+                        </Col>
+                      </Row>
+                    </div>
+                    <Row gutter={8}>
+                      { items.map((item, i) => (
+                        <Col key={i}>
+                          <div className={`carousel-pointer ${selectedItemIndex === i ? 'selected' : ''}`}/>
+                        </Col>
+                      ))}
+                    </Row>
                   </Col>
                 </Row>
-              </Col>
-            </Row>
+              </div>
+            )) }
+            <FaChevronLeft onClick={handleClickLeft} className='carousel-left-icon' />
+            <FaChevronRight onClick={handleClickRight} className='carousel-right-icon' />
           </div>
-        )) }
-        <FaChevronLeft onClick={handleClickLeft} className='carousel-left-icon' />
-        <FaChevronRight onClick={handleClickRight} className='carousel-right-icon' />
+        </div>
       </div>
   );
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,18 +2,27 @@ import Carousel from 'components/Carousel';
 import './Home.less';
 
 const featuredNews = [
-  'gas1',
-  'gas2'
+  {
+    imagePath: 'https://media.comicbook.com/2020/09/edens-zero-anime-poster-hiro-mashima-fairy-tail-creator-1238508-1280x0.jpeg',
+    title: `Edens Zero Anime's Video Reveals More Cast, T.M.Revolution Song, Netflix 2021 Streaming`,
+    description: 'Sayaka Ohara, Houchu Ohtsuka, Kikuko Inoue join cast; CHICO with Honeyworks perform ending'
+  },
+  {
+    imagePath: 'https://cdn.animenewsnetwork.com/thumbnails/max650x650/cms/news.4/170552/madoka-magica-10.jpg',
+    title: 'Madoka Magica Anime Gets 10th Anniversary Event on April 25',
+    description: `Director Junichirō Taniguchi's 10th anniversary illustration unveiled`,
+  },
+  {
+    imagePath: 'https://cdn.trinikid.com/109514/uploads/7a192750-f68a-11ea-9adf-d9b634015f55_800_420.jpeg',
+    title: `A Tale of Two Redheads: Yona vs. Shirayuki`,
+    description: 'Making comparisons amongst different facets of popular anime is a fun little exercise I like to engage with occasionally, especially when certain trends end up getting clustered together. In one such instance back in 2015, we coincidentally saw two different anime adaptations of shoujo manga with red-haired female protagonists in fantasy settings, and the general similarities continue well beyond that point, so today, I wanted to take a closer look at Yona from Yona of the Dawn and Shirayuki from Snow White with the Red Hair to see how they compare and what makes they work within their respective stories.'
+  },
 ]
 
 const Home = () => {
   return (
     <div>
-      <div className='home-header-section'>
-        <div className='content-container home-carousel-container'>
-          <Carousel items={featuredNews}/>
-        </div>
-      </div>
+      <Carousel items={featuredNews}/>
     </div>
   )
 }
