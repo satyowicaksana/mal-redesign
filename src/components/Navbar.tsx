a04a9f7186bd16a9ff59c0a2bb3f772e2bcdfc1c
satyowicaksana <satyo.wicaksana@rebelworks.co>
2021-03-14 17:04:59 +0700

    add popover, search, and sign in button slicing in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
-import { Row, Col, Typography } from 'antd';
+import { useState } from 'react';
+import { Row, Col, Typography, Popover, Input, Button } from 'antd';
+import { FaSearch } from 'react-icons/fa';
+import { BiLogInCircle } from 'react-icons/bi';
+
 import './Navbar.less';
 
-const { Title, Link } = Typography;
+const { Title, Text, Link } = Typography;
 
 const links = [
   {
@@ -28,20 +32,50 @@ const links = [
 ]
 
 const Navbar = () => {
+  const [search, setSearch] = useState('');
+
   return (
     <div className='navbar-wrapper'>
       <div className='content-container'>
         <Row align='middle' justify='space-between'>
           <Col>
-            <Title type='secondary' level={3}>MyAnimeList</Title>
+            <Title type='secondary' level={3} className='navbar-logo'>MyAnimeList</Title>
           </Col>
           <Col>
-            <Row gutter={16}>
+            <Row gutter={16} align='middle'>
               { links.map((link, i) => (
-                <Col key={i}>
-                  <Link type='secondary' strong>{link.text}</Link>
-                </Col>
+                <Popover
+                  placement='bottomLeft'
+                  content={
+                    <div className='py-2'>
+                      <div className='navbar-popover-option-container py-1 px-3'>
+                        <Text>Option</Text>
+                      </div>
+                      <div className='navbar-popover-option-container py-1 px-3'>
+                        <Text>Option Long</Text>
+                      </div>
+                      <div className='navbar-popover-option-container py-1 px-3'>
+                        <Text>Option</Text>
+                      </div>
+                    </div>
+                  }
+                >
+                  <Col key={i}>
+                    <Link type='secondary' strong>{link.text}</Link>
+                  </Col>
+                </Popover>
               )) }
+              <Col>
+                <Input
+                  value={search}
+                  onChange={e => setSearch(e.target.value)}
+                  suffix={<FaSearch className='navbar-search-icon'/>}
+                  className={`navbar-search ${search ? 'expanded' : ''}`}
+                />
+              </Col>
+              <Col>
+                <Button type='default' icon={<BiLogInCircle className='mr-1'/>}>SIGN IN</Button>
+              </Col>
             </Row>
           </Col>
         </Row>
